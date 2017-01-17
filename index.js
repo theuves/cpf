9d0b5766e5dc8837e6dbc0410e50a371c3853753
Matheus Alves <theuves@outlook.com>
2017-01-17 19:38:48 -0300

    Faz mais algumas coisas

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
 'use strict';
 
-var shuffleList = require('shuffle-list');
-
 function dv(cpf) {
     cpf = cpf.reverse();
-    var v1 = 0,
-        v2 = 0;
+    var v1 = 0, v2 = 0;
 
-    var i = 0;
-
-    while (i < 9) {
+    for (var i = 0; i < 9; i++) {
         v1 = v1 + cpf[i] * (9 - (i % 10));
         v2 = v2 + cpf[i] * (9 - ((i + 1) % 10));
-        i = i + 1;
     }
 
     v1 = (v1 % 11) % 10;
-    v2 = v2 + v1 * 9;
-    v2 = (v2 % 11) % 10;
+    v2 = ((v2 + v1 * 9) % 11) % 10;
 
     return [v1, v2].join('');
 }
 
 function format(cpf) {
-    if (cpf === undefined) {
-        return '';
-    }
+    if (!cpf) return '';
 
     cpf = cpf.toString().replace(/\D+/g, '');
-
     cpf = cpf.replace(/(\d{1,3})(\d{1,3})?(\d{1,3})?(\d{1,2})?/, '$1.$2.$3-$4');
-
-    //Removes simbols at the end
-    //Needed for backspace in browsers
     cpf = cpf.replace(/(\D+)$/, '');
 
     return cpf;
 }
 
 function generate() {
-    var list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        numbers = shuffleList(list).splice(0, 9);
+    var shuffleList = require('shuffle-list');
+    var list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var numbers = shuffleList(list).splice(0, 9);
 
     return format(numbers.join('') + dv(numbers));
 }
 
 function validate(cpf) {
     cpf = cpf.toString();
-
-    if (/^\d{11}$/.test(cpf)) {
-        cpf = format(cpf);
-    }
+    if (/^\d{11}$/.test(cpf)) cpf = format(cpf);
 
     var re = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
@@ -61,8 +46,8 @@ function validate(cpf) {
             return false;
         }
 
-        var digs = cpf.split('-')[1],
-            nums = cpf.split('-')[0].match(/\d/g);
+        var digs = cpf.split('-')[1];
+        var nums = cpf.split('-')[0].match(/\d/g);
 
         return dv(nums) === digs
     } else {
