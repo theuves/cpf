793ff02679ea955ddb95ca0da09ed03bf3b17a68
Matheus Alves <theuves@outlook.com>
2017-07-05 17:04:38 -0400

    várias alterações

--- index.js
+++ index.js
@@ -1,62 +1,11 @@
-'use strict';
+"use strict";
 
-function dv(cpf) {
-    cpf = cpf.reverse();
-    var v1 = 0, v2 = 0;
-
-    for (var i = 0; i < 9; i++) {
-        v1 = v1 + cpf[i] * (9 - (i % 10));
-        v2 = v2 + cpf[i] * (9 - ((i + 1) % 10));
-    }
-
-    v1 = (v1 % 11) % 10;
-    v2 = ((v2 + v1 * 9) % 11) % 10;
-
-    return [v1, v2].join('');
-}
-
-function format(cpf) {
-    if (!cpf) return '';
-
-    cpf = cpf.toString().replace(/\D+/g, '');
-    cpf = cpf.replace(/(\d{1,3})(\d{1,3})?(\d{1,3})?(\d{1,2})?/, '$1.$2.$3-$4');
-    cpf = cpf.replace(/(\D+)$/, '');
-
-    return cpf;
-}
-
-function generate() {
-    var shuffleList = require('shuffle-list');
-    var list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var numbers = shuffleList(list).splice(0, 9);
-
-    return format(numbers.join('') + dv(numbers));
-}
-
-function validate(cpf) {
-    cpf = cpf.toString();
-    if (/^\d{11}$/.test(cpf)) cpf = format(cpf);
-
-    var re = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-
-    if (re.test(cpf)) {
-        var regex = /^(1{11}|2{11}|3{11}|4{11}|5{11}|6{11}|7{11}|8{11}|9{11})$/;
-
-        if (regex.test(cpf.replace(/\.|-/g, ''))) {
-            return false;
-        }
-
-        var digs = cpf.split('-')[1];
-        var nums = cpf.split('-')[0].match(/\d/g);
-
-        return dv(nums) === digs
-    } else {
-        return false;
-    }
-}
+var format = require("./lib/format.js");
+var generate = require("./lib/generate.js");
+var validate = require("./lib/validate.js");
 
 module.exports = {
-    format: format,
-    generate: generate,
-    validate: validate
+  format: format,
+  generate: generate,
+  validate: validate
 };
--- test.js
+++ test.js
@@ -1,34 +1,28 @@
-var assert = require('assert');
-var CPF = require('./');
+"use strict";
 
-describe('Tests', function () {
-    it('.format(cpf)', function () {
-        assert.equal(CPF.format('111444'), '111.444');
-        assert.equal(CPF.format('1114447'), '111.444.7');
-        assert.equal(CPF.format('11144477'), '111.444.77');
-        assert.equal(CPF.format('111444777'), '111.444.777');
-        assert.equal(CPF.format('1114447773'), '111.444.777-3');
-        assert.equal(CPF.format('11144477735'), '111.444.777-35');
-        assert.equal(CPF.format('111-444-777-35'), '111.444.777-35');
-        assert.equal(CPF.format('111'), '111');
-        assert.equal(CPF.format('1114'), '111.4');
-        assert.equal(CPF.format('111.4'), '111.4');
-        assert.equal(CPF.format('111.'), '111');
-        assert.equal(CPF.format('my cpf'), '');
-        assert.equal(CPF.format('111 4'), '111.4');
-    });
+var assert = require("assert");
+var CPF = require("./");
 
-    it('.validate(cpf)', function () {
-        assert.equal(CPF.validate('11144477735'), true);
-        assert.equal(CPF.validate('111.444.777-35'), true);
-        assert.equal(CPF.validate('111.444.777-3'), false);
-        assert.equal(CPF.validate('111.444.777-34'), false);
-        assert.equal(CPF.validate('111.444.777-355'), false);
-    });
+describe("Tests", function () {
+  it(".format()", function () {
+    assert.equal(CPF.format("11144477735"), "111.444.777-35");
+    assert.equal(CPF.format("111444777-35"), "111.444.777-35");
+    assert.equal(CPF.format("111.444.77735"), "111.444.777-35");
+    assert.equal(CPF.format("111.444.777-35"), "111.444.777-35");
+    assert.equal(CPF.format("111.444.777.35"), null);
+  });
 
-    it('.generate()', function () {
-        assert.equal(CPF.validate(CPF.generate()), true);
-        assert.equal(CPF.generate().length, 14);
-        assert.equal(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(CPF.generate()), true);
-    });
+  it(".validate()", function () {
+    assert.equal(CPF.validate("11144477735"), true);
+    assert.equal(CPF.validate("111.444.777-35"), true);
+    assert.equal(CPF.validate("111.444.777-3"), false);
+    assert.equal(CPF.validate("111.444.777-34"), false);
+    assert.equal(CPF.validate("111.444.777-355"), false);
+  });
+
+  it(".generate()", function () {
+    assert.equal(CPF.validate(CPF.generate()), true);
+    assert.equal(CPF.generate().length, 14);
+    assert.equal(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(CPF.generate()), true);
+  });
 });
