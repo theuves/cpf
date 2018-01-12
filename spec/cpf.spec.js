011ce0cc821b8c9927c54c28f71affde6cb53c16
Matheus Alves <theuves@outlook.com>
2018-01-11 23:08:49 -0300

    melhora testes

--- spec/cpf.spec.js
+++ spec/cpf.spec.js
@@ -3,71 +3,85 @@
 const CPF = require("../");
 
 describe("CPF", () => {
-  it("calcDv", () => {
-    const DV = [3, 5];
-    const EM_ARR = [1, 1, 1, 4, 4, 4, 7, 7, 7];
-    const EM_STR = "111444777";
+  let cpfSoNumeros;
+  let cpfFormatado;
+  let cpfDesformatado;
+  let cpfSemiFormatado;
+  let cpfComTamanhoValido;
+  let cpfInvalido;
+  let reCpfFormatado;
+  let reCpfDesformatado;
 
-    const dvDeArr = CPF.calcDv(EM_ARR);
-    const dvDeStr = CPF.calcDv(EM_STR);
-
-    expect(dvDeStr).toEqual(DV);
-    expect(dvDeStr).toEqual(DV);
+  beforeEach(() => {
+    cpfFormatado = "111.444.777-35";
+    cpfDesformatado = "11144477735";
+    cpfSemiFormatado = "111444777-35";
+    cpfComTamanhoValido = "00000000000";
+    cpfInvalido = "111.444.777-53";
+    reCpfFormatado = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    reCpfDesformatado = /^\d{11}$/;
   });
 
-  it("clear", () => {
-    const CPF_FORMATADO = "111.444.777-35";
-    const CPF_FORMATADO_TRACO = "111444777-35";
-    const CPF_DESFORMATADO = "11144477735";
+  describe("calcDev(digits)", () => {
+    let dv;
+    let digitosEmArray;
+    let digitosEmString;
 
-    const cpfFormatado = CPF.clear(CPF_FORMATADO);
-    const cpfDesformatado = CPF.clear(CPF_DESFORMATADO);
-    const cpfFormatadoTraco = CPF.clear(CPF_FORMATADO_TRACO);
+    beforeEach(() => {
+      dv = [3,5];
+      digitosEmArray = [1,1,1,4,4,4,7,7,7];
+      digitosEmString = "111444777";
+    });
 
-    expect(cpfFormatado).toBe(CPF_DESFORMATADO);
-    expect(cpfDesformatado).toBe(CPF_DESFORMATADO);
-    expect(cpfFormatadoTraco).toBe(CPF_DESFORMATADO);
+    it("deve obter os dígitos verificadores a partir de array", () => {
+      expect(CPF.calcDv(digitosEmArray)).toEqual(dv);
+    });
+    it("deve obter os dígitos verificadores a partir de string", () => {
+      expect(CPF.calcDv(digitosEmString)).toEqual(dv);
+    });
   });
 
-  it("format", () => {
-    const CPF_FORMATADO = "111.444.777-35";
-    const CPF_DESFORMATADO = "11144477735";
-
-    const cpfFormatado = CPF.format(CPF_FORMATADO);
-    const cpfDesformatado = CPF.format(CPF_DESFORMATADO);
-
-    expect(cpfFormatado).toBe(CPF_FORMATADO);
-    expect(cpfDesformatado).toBe(CPF_FORMATADO);
+  describe("clear(cpf)", () => {
+    it("deve remover qualquer caractere não-dígito", () => {
+      expect(CPF.clear(cpfFormatado)).toBe(cpfDesformatado);
+      expect(CPF.clear(cpfDesformatado)).toBe(cpfDesformatado);
+      expect(CPF.clear(cpfSemiFormatado)).toBe(cpfDesformatado);
+    });
   });
 
-  it("isValid", () => {
-    const CPF_FORMATADO = "111.444.777-35";
-    const CPF_DESFORMATADO = "11144477735";
-    const CPF_COM_TAMANHO_VALIDO = "00000000000";
-    const CPF_DA_DILMA_ROUSSEFF = "133.267.246-91";
-
-    const cpfFormatado = CPF.isValid(CPF_FORMATADO);
-    const cpfDesformatado = CPF.isValid(CPF_DESFORMATADO);
-    const cpfComTamanhoValido = CPF.isValid(CPF_COM_TAMANHO_VALIDO);
-    const cpfDaDilmaRousseff = CPF.isValid(CPF_DA_DILMA_ROUSSEFF);
-
-    expect(cpfFormatado).toBe(true);
-    expect(cpfDesformatado).toBe(true);
-    expect(cpfComTamanhoValido).toBe(true);
-    expect(cpfDaDilmaRousseff).toBe(true);
+  describe("format(cpf)", () => {
+    it("deve formatar número de cpf formatado", () => {
+      expect(CPF.format(cpfFormatado)).toBe(cpfFormatado);
+    });
+    it("deve formatar número de cpf desformatado", () => {
+      expect(CPF.format(cpfDesformatado)).toBe(cpfFormatado);
+    });
   });
 
-  it("generate", () => {
-    const cpfFormatado = CPF.generate();
-    const cpfDesformatado = CPF.generate(true);
-    const cpfInvalido = CPF.generate(undefined, true);
+  describe("isValid(cpf[, length])", () => {
+    it("deve validar número de cpf formatado", () => {
+      expect(CPF.isValid(cpfFormatado)).toBeTruthy();
+    });
+    it("deve validar número de cpf desformatado", () => {
+      expect(CPF.isValid(cpfDesformatado)).toBeTruthy()
+    });
+    it("deve validar número de cpf somente pelo tamanho", () => {
+      expect(CPF.isValid(cpfComTamanhoValido, true)).toBeTruthy();
+    });
+  });
 
-    const ehValidoCpfFormatado = CPF.isValid(cpfFormatado);
-    const ehValidoCpfDesformatado = CPF.isValid(cpfDesformatado);
-    const ehValidoCpfInvalido = CPF.isValid(cpfInvalido);
-
-    expect(ehValidoCpfFormatado).toBe(true);
-    expect(ehValidoCpfDesformatado).toBe(true);
-    expect(ehValidoCpfInvalido).not.toBe(true);
+  describe("generate([formatted][, invalid])", () => {
+    it("deve gerar número de cpf formatado e válido", () => {
+      expect(CPF.generate()).toMatch(reCpfFormatado);
+      expect(CPF.isValid(CPF.generate())).toBeTruthy()
+    });
+    it("deve gerar número de cpf desformatado e valido", () => {
+      expect(CPF.generate(false)).toMatch(reCpfDesformatado);
+      expect(CPF.isValid(CPF.generate(false))).toBeTruthy();
+    });
+    it("deve gerar número de cpf formatado e inválido", () => {
+      expect(CPF.generate(undefined, true)).toMatch(reCpfFormatado);
+      expect(CPF.isValid(CPF.generate(undefined, true))).toBeFalsy();
+    });
   });
 });
