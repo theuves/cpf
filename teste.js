49dc6bcf7fa4bb06a9ca06e5611437f9abc02277
mateus alves <theuves@gmail.com>
2018-04-29 18:17:19 -0400

    melhora o build

--- teste.js
+++
@@ -1,3 +0,0 @@
-const cpf = require('./build/index');
-
-console.log(cpf.isValid('11111111111'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
-const path = require('path');
-const webpack = require('webpack');
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+const pkg = require("./package.json");
+const {name, version, author} = pkg;
+const year = (new Date()).getFullYear();
+const banner = `${name} v${version} | (c) ${year} by ${author}`;
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        library: 'cpf',
-        libraryTarget: 'commonjs2',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
+  entry: {
+    "piii": "./index.js",
+    "piii.min": "./index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    library: "CPF",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/
+    }),
+    new webpack.BannerPlugin(banner)
+  ],
+  stats: {
+    colors: true
+  }
 };
