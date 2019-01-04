4cff7b5c3f717a88cb7f94a3efb9f5c8de8f2c96
Matheus Alves <theuves@gmail.com>
2019-01-04 16:15:09 -0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,11 @@
 declare module 'cpf' {
-  export function calcDv(cpf: string): string[]
-  export function calcDv(cpf: string[]): string[]
-
-  export function clear(cpf: string): string
-
-  export function format(cpf: string): string
-  export function format(cpf: string): string
+  export function format(cpfNumber: string): string
 
   export function generate(): string
-  export function generate(formatted:boolean): string
-  export function generate(formatted:boolean, invalid:boolean): string
+  export function generate(formatted: boolean): string
+  export function generate(formatted: boolean, invalid: boolean): string
 
-  export function isValid(cpf:string): boolean
-  export function isValid(cpf:string, byLength: boolean): boolean
+  export function isValid(cpfNumber: string): boolean
+  export function isValid(cpfNumber: string, byLength: boolean): boolean
 }
 
