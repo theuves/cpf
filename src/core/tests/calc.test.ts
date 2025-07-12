114e5512e8204280a5566d8e5ff09d7942288d51
Matheus Alves <theuves@gmail.com>
2025-07-12 11:20:52 +0200

    Add calc function and corresponding tests

---
+++ src/core/tests/calc.test.ts
@@ -0,0 +1,41 @@
+import test from 'ava';
+import { calc } from '../calc';
+
+test('calc should return correct verifiers for valid body', t => {
+  const body = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const result = calc(body);
+  t.deepEqual(result, [0, 9]);
+});
+
+test('calc should throw error for body with less than 9 digits', t => {
+  const body = [1, 2, 3, 4, 5, 6, 7, 8];
+  const error = t.throws(() => {
+    calc(body);
+  }, { instanceOf: Error });
+  t.is(error.message, 'Input must be exactly 9 digits');
+});
+
+test('calc should throw error for body with more than 9 digits', t => {
+  const body = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const error = t.throws(() => {
+    calc(body);
+  }, { instanceOf: Error });
+  t.is(error.message, 'Input must be exactly 9 digits');
+});
+
+test('calc should throw error for body with non-integer values', t => {
+  const body = [1, 2, 3, 4, 5, 6, 7, 8, 'a'];
+  const error = t.throws(() => {
+    // @ts-ignore
+    calc(body);
+  }, { instanceOf: Error });
+  t.is(error.message, 'All elements must be integers between 0 and 9');
+});
+
+test('calc should throw error for body with integers out of range', t => {
+  const body = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+  const error = t.throws(() => {
+    calc(body);
+  }, { instanceOf: Error });
+  t.is(error.message, 'All elements must be integers between 0 and 9');
+});
