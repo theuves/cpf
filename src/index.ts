9636d0f52e0ac5c37582d78086dca55858ef0ac4
Matheus Alves <theuves@gmail.com>
2025-07-12 09:41:04 +0200

    Remove unused CPF type definitions from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,3 @@ export { format } from './format';
 export { generate } from './generate';
 export { isValid } from './is-valid';
 export { getCD } from './get-cd';
-
-// Tipos para o módulo
-export interface CPFGeneratorOptions {
-  formatted?: boolean;
-  invalid?: boolean;
-}
-
-export interface CPFValidatorOptions {
-  byLength?: boolean;
-}
