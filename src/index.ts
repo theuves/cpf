566c3984db88626af9743e555f05d3c79055a6e6
Matheus Alves <theuves@gmail.com>
2025-07-20 11:35:59 +0200

    Improve test structure and remove unnecessary semicolons

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+// cpf
+export * from './calc'
+export * from './check'
+export * from './format'
+export * from './generate'
+export * from './repair'
+export * from './rfs'
+export * from './unformat'
+export * from './validate'
