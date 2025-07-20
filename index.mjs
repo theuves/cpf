058c7b167ea1f65d39dcc448ba13f463bcbb10ea
Matheus Alves <theuves@gmail.com>
2025-07-20 11:33:24 +0200

    Remove CommonJS entry point and type definitions

--- index.mjs
+++
@@ -1,9 +0,0 @@
-// CommonJS entry point for backward compatibility
-const { format, generate, isValid, getCD } = require('./dist/cpf.min.js')
-
-module.exports = {
-  format,
-  generate,
-  isValid,
-  getCD,
-}
